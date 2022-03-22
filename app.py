5c60959f6f911a58a8be00ff4c6a6b6337d133e4
EdvandroM <42396910+EdvandroM@users.noreply.github.com>
2022-03-21 21:05:08 -0300

    simulated deploy

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"   
+    return "Laboratório Pipeline DevOps - FIAP Deploy QA/Prod"   
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
