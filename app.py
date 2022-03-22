f23158c0bcc6527907a0ef062e00ec66a57e90af
EdvandroM <42396910+EdvandroM@users.noreply.github.com>
2022-03-21 21:13:49 -0300

    fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - FIAP Deploy QA/Prod"   
+    return "Laboratório Pipeline DevOps"   
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
